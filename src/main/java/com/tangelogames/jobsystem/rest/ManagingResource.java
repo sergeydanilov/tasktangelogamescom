30c05f7774830a1567fe51023cf8bc218758d7b1
Sergey Danilov <sergeydanilov@gmail.com>
2022-05-16 16:57:49 +0300

    added uni combine

--- src/main/java/com/tangelogames/jobsystem/rest/ManagingResource.java
+++ src/main/java/com/tangelogames/jobsystem/rest/ManagingResource.java
@@ -1,6 +1,9 @@
 package com.tangelogames.jobsystem.rest;
 
 import com.tangelogames.jobsystem.base.AbstractScheduledJob;
+import com.tangelogames.jobsystem.jobs.CustomJob1;
+import com.tangelogames.jobsystem.jobs.CustomJob2;
+import io.smallrye.common.annotation.Blocking;
 import io.smallrye.mutiny.Uni;
 import io.vertx.mutiny.core.eventbus.EventBus;
 import io.vertx.mutiny.core.eventbus.Message;
@@ -12,9 +15,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Log4j2
 @Path("/managing")
@@ -26,30 +28,56 @@ public class ManagingResource {
     @Inject
     Instance<AbstractScheduledJob> jobs;
 
+    @Blocking
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/status")
-    public Uni<List<String>> statusAll(String name) {
+    public Uni<Map<String, String>> statusAll(String name) {
 //        bus.<String>request(name, "status").onItem().;
 
-        List<Uni<String>> unisPerJob = new ArrayList<>();
-        jobs.forEach(job -> {
-            var statusOfTheJob = bus.<String>request(name, "status").onItem().transform(Message::body);
-            unisPerJob.add(statusOfTheJob);
-        });
-        final int size = unisPerJob.size();
-        Uni[] arrayOfUnis = new Uni[size];
+        final Uni<String> status1 = bus.<String>request("com.tangelogames.jobsystem.jobs.CustomJob1", "status").onItem().transform(Message::body);
+        final Uni<String> status2 = bus.<String>request("com.tangelogames.jobsystem.jobs.CustomJob2", "status").onItem().transform(Message::body);
 
-        return Uni.combine()
-                .all().unis(arrayOfUnis).combinedWith(
+//        final Uni<String> status1 = Uni.createFrom().item("Hello1");
+//        final Uni<String> status2 = Uni.createFrom().item("Hello2");
+
+
+//        return Uni.combine()
+//                .all().unis(status1, status2).asTuple();
+
+
+        Uni<Map<String, String>> uni = Uni.combine()
+                .all().unis(status1, status2).combinedWith(
                         listOfResponses -> {
-                            List<String> list = new LinkedList<String>();
-                            for (int i = 0; i < size; i++) {
-                                list.add((String) listOfResponses.get(i));
-                            }
-                            return list;
+                            Map<String, String> map = new LinkedHashMap<>();
+                            map.put(CustomJob1.class.getCanonicalName(), (String) listOfResponses.get(0));
+                            map.put(CustomJob2.class.getCanonicalName(), (String) listOfResponses.get(1));
+                            return map;
                         }
                 );
+
+        return uni;
+
+//        List<Uni<String>> unisPerJob = new ArrayList<>();
+//        jobs.forEach(job -> {
+//            var statusOfTheJob = bus.<String>request(name, "status").onItem().transform(Message::body);
+//            unisPerJob.add(statusOfTheJob);
+//        });
+//        final int size = unisPerJob.size();
+//        Uni[] arrayOfUnis = new Uni[size];
+//
+//        final Uni<List<String>> listUni = Uni.combine()
+//                .all().unis(arrayOfUnis).combinedWith(
+//                        listOfResponses -> {
+//                            List<String> list = new LinkedList<>();
+//                            for (int i = 0; i < size; i++) {
+//                                list.add((String) listOfResponses.get(i));
+//                            }
+//                            return list;
+//                        }
+//                );
+
+//        return listUni;
 //
 //
 //        List<String> strings = new ArrayList<>();
